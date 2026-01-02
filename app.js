43990057881d5a7b1bc0721191009b15a619d035
Mirjhan <mirjhanmoreyra@gmail.com>
2026-01-02 18:18:07 -0500

    feat: JYMBACK-11 configuraciones middleware

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
   res.send('Hola mundo!')
